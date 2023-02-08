e2850f88e5d3089e5626f0ebfc14c66d4ded3ef4
taylor-w <taylorwood39@gmail.com>
2023-02-07 18:12:54 -0600

    Wrap tiktok.py in try except

--- app/tiktok.py
+++ app/tiktok.py
@@ -5,21 +5,24 @@ from twitch import *
     # TODO: parameterize config (def build_config ?)
 
 def tiktok_chat(res_list):
-    print("tiktok_chat start")
-    client: TikTokLiveClient = TikTokLiveClient(unique_id="@some_channel", **({
-        "loop": "loop"
-    }))
+    try:
+        print("tiktok_chat start")
+        client: TikTokLiveClient = TikTokLiveClient(unique_id="@imcoty", **({
+            "loop": "loop"
+        }))
 
-    # Notice no decorator?
-    @client.on("comment")
-    async def on_comment(event: CommentEvent):
-        # add tiktok msg to list
-        res_list.append(f"{event.user.nickname}: {event.comment}")
-        # send tiktok msg list to twitch_rfc to process
-        twitch_rfc(res_list)
-        # remove tiktok msg from list so it's sent again
-        res_list.remove(f"{event.user.nickname}: {event.comment}")
+        # Notice no decorator?
+        @client.on("comment")
+        async def on_comment(event: CommentEvent):
+            # add tiktok msg to list
+            res_list.append(f"{event.user.nickname}: {event.comment}")
+            # send tiktok msg list to twitch_rfc to process
+            twitch_rfc(res_list)
+            # remove tiktok msg from list so it's sent again
+            res_list.remove(f"{event.user.nickname}: {event.comment}")
 
-    # Run the client and block the main thread
-    # await client.start() to run non-blocking
-    client.run()
+        # Run the client and block the main thread
+        # await client.start() to run non-blocking
+        client.run()
+    except Exception as ex:
+        print(ex)
