89d8620c516818ee2ad6ab25dba3fe7e04a7b13e
taylor-w <taylorwood39@gmail.com>
2023-02-10 20:18:36 -0600

    Improve failover behavior of main

--- app/main.py
+++ app/main.py
@@ -7,14 +7,20 @@ from tiktok import *
 
 def main():
     sys.tracebacklimit = 0
-    try:
-        res_list = []
-        # 1 thread to async run tiktok_chat on res_list
-        t1 = threading.Thread(target=asyncio.run, args=(tiktok_chat(res_list), ))
-        t1.start()
-        t1.join()
-    except asyncio.CancelledError:
-        print("Error in coroutine, ending main")
+    res_list = []
+    # 1 thread to async run tiktok_chat on res_list
+    t1 = threading.Thread(target=asyncio.run, args=(tiktok_chat(res_list), ))
+
+    while True:
+        try:
+            t1.start()
+            t1.join()
+        except asyncio.CancelledError:
+            print("Error in coroutine, ending main")
+        if t1.is_alive():
+            continue
+        else:
+            break
 
 if __name__ == '__main__':
     # asyncio set event loop policy for windows compliance
