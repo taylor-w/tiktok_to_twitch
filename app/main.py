8409e5e589845cbb8af3ff63587ba3d30dd02402
taylor-w <taylorwood39@gmail.com>
2023-03-03 19:57:15 -0600

    Add rep structure and backoff counter to handle tiktok user being offline

--- app/main.py
+++ app/main.py
@@ -2,25 +2,43 @@ import asyncio
 import sys
 import threading
 from tiktok import *
+from time import sleep
 
 # TODO: Add 'debug' mode in config where 'debug' substitutes bot's channel and a random tiktok live, for testing
 
 def main():
     sys.tracebacklimit = 0
     res_list = []
-    # 1 thread to async run tiktok_chat on res_list
-    t1 = threading.Thread(target=asyncio.run, args=(tiktok_chat(res_list), ))
+    sleep_counter = 0
+    backoff_seconds = 60
 
     while True:
-        try:
-            t1.start()
-            t1.join()
-        except asyncio.CancelledError:
-            print("Error in coroutine, ending main")
-        if t1.is_alive():
-            continue
-        else:
-            break
+        while True:
+            try:
+                # 1 thread to async run tiktok_chat on res_list
+                t1 = threading.Thread(target=asyncio.run, args=(tiktok_chat(res_list), ))
+                t1.start()
+                t1.join()
+            except asyncio.CancelledError:
+                print("Error in coroutine, ending main")
+            if t1.is_alive():
+                continue
+            else:
+                # 1. 60 secs, 2. 120 sec, 3. 440 secs, 4. 880 secs, 5. 1760 secs, 6. 3600 secs
+                if sleep_counter == 0:
+                    backoff_seconds = 60
+                elif sleep_counter == 1:
+                    backoff_seconds = backoff_seconds * 2
+                elif sleep_counter == 2:
+                    backoff_seconds = backoff_seconds * 3
+                elif sleep_counter == 3:
+                    backoff_seconds = backoff_seconds * 4
+                else:
+                    backoff_seconds = 3600
+                print(f'Tiktok user offline, sleeping thread: {backoff_seconds}...')
+                sleep(backoff_seconds)
+                sleep_counter += 1
+                break
 
 if __name__ == '__main__':
     # asyncio set event loop policy for windows compliance
