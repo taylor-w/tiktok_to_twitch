902e505a28e47270eda0e7022c50bbf9cc490ac6
taylor-w <taylorwood39@gmail.com>
2023-04-07 15:50:32 -0500

    Update build_config() and config reference in .gitignore

--- app/build_config.py
+++
@@ -1,8 +0,0 @@
-import yaml
-
-def build_config():
-    try:
-        config = yaml.safe_load(open("config.yaml"))
-        return config
-    except Exception as ex:
-        print(f'Config file error: {ex}')
--- app/utility.py
+++ app/utility.py
@@ -1,4 +1,15 @@
 import threading
+import yaml
+from pathlib import Path
+
+def build_config():
+    try:
+        my_path = Path(__file__).resolve()  # resolve to get rid of any symlinks
+        config_path = my_path.parent / 'config.yaml'
+        config = yaml.safe_load(open(config_path))
+        return config
+    except Exception as ex:
+        print(f'Config file error: {ex}')
 
 def debug_thread(fxn_name):
     print(f'{fxn_name}: {threading.get_ident()}')
